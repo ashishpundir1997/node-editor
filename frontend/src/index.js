2e28e0127512c6211d78b3710086d211aba49f2a
ashishpundir1997 <ashishpundir1997@gmail.com>
2025-11-15 16:25:23 +0530

    Restore lost frontend package.json and CSS sources (input.css, index.css) and wire output.css import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Load compiled Tailwind output plus small global overrides
+import './output.css';
 import './index.css';
 import App from './App';
 
